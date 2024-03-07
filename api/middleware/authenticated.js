ea71b3d43cd28eaf241d40a9e5fb4e751e353ffe
kspencerl <kimberly.liz@sga.pucminas.br>
2024-03-07 13:53:50 -0300

    criação do middleware para validação do token [ , secret, ]

--- api/middleware/authenticated.js
+++ api/middleware/authenticated.js
@@ -1,18 +1,30 @@
-//middleware para verificar se o token é válido
+//criação do middleware para verificar se o token é válido
 
-const { verify } = require('jsonwebtoken')
+const { verify, decode } = require('jsonwebtoken')
+//resgatando nossa secret
 const jsonSecret = require('../config/jsonSecret')
+
 module.exports = async (req, res, next) =>{
     const token = req.headers.authorization
 
-    if (token) {
-        res.status(401).send('access token não informado')
+    if (token) { 
+        res.status(401).send('Access token não informado')
     }
-
+    //quebrar token
     const [,accessToken] = token.split(" ")
+            //pegar segunda variável
 
     try {
-        //validar se token possui o secret
+        //validar se o secret é o correto
+        verify(accessToken, jsonSecret.secret)
+
+             //desestruturação do token
+        const { id, email } = await decode(accessToken)
+        
+        req.userId = id
+        req.userEmail = email
+
+        return next 
 
     } catch (error) {
         res.status(401).send('Usuário não autorizado')
